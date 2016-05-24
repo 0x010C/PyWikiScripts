d71fd3e6e6979c75bf8ecece9385d022c25ae3a8
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2016-05-24 17:40:59 +0200

    Threading the process

--- external_links_checker.py
+++ external_links_checker.py
@@ -6,51 +6,95 @@
 
 import pywiki
 import requests
+import threading
+import time
+from collections import deque
+
+
+lock = threading.Lock()
+links = deque()
+
+class LinkChecker (threading.Thread):
+	def __init__(self, threadID):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+	def run(self):
+		print "Starting " + str(self.threadID)
+		while True:
+			with lock:
+				try:
+					(title, link) = links.popleft()
+					print str(self.threadID)+"#"+str(len(links))
+				except IndexError:
+					print "pause"+str(self.threadID)
+					link = None
+			if link == None:
+				time.sleep(2)
+				continue
+			
+			try:
+				r = requests.get(link)
+				if r.status_code != 200:
+					print title+" --> ("+str(r.status_code)+") "+link
+			except requests.exceptions.ConnectionError:
+				print title+" --> (DOM) "+link
+			except requests.exceptions.TooManyRedirects:
+				print title+" --> (RED) "+link
+			
+		print "Exiting " + str(self.threadID)
 
 
 # Main
 def main():
+	global links
 	pw = pywiki.Pywiki("frwiki-NeoBOT")
 	pw.login()
 	pw.limit = 500
 	
+	threads = []
+	for i in range(0,64):
+		threads.append(LinkChecker(i))
+		threads[i].start()
+	
 	gap_continue = ""
 	le_offset = 0
 	while gap_continue != None:
-		response = pw.request({
-			"action":"query",
-			"format":"json",
-			"prop":"extlinks",
-			"generator":"allpages",
-			"elexpandurl":"1",
-			"ellimit":pw.limit,
-			"gaplimit":pw.limit,
-			"eloffset":le_offset,
-			"gapcontinue":gap_continue,
-			"assert":pw.assertion,
-		})
+		need_more_links = False
+		with lock:
+			if len(links) < 200:
+				need_more_links = True
+		
+		if need_more_links:
+			print "Get more links"
+			response = pw.request({
+				"action":"query",
+				"format":"json",
+				"prop":"extlinks",
+				"generator":"allpages",
+				"elexpandurl":"1",
+				"ellimit":pw.limit,
+				"gaplimit":pw.limit,
+				"eloffset":le_offset,
+				"gapcontinue":gap_continue,
+				"assert":pw.assertion,
+			})
 
-		for id in response["query"]["pages"]:
-			if "extlinks" in response["query"]["pages"][id]:
-				#print response["query"]["pages"][id]["title"]
-				for extlink in response["query"]["pages"][id]["extlinks"]:
-					try:
-						r = requests.get(extlink["*"])
-						if r.status_code != 200:
-							print response["query"]["pages"][id]["title"]+" --> ("+str(r.status_code)+") "+extlink["*"]
-					except requests.exceptions.ConnectionError:
-						print response["query"]["pages"][id]["title"]+" --> (DOM) "+extlink["*"]
-					except requests.exceptions.TooManyRedirects:
-						print response["query"]["pages"][id]["title"]+" --> (RED) "+extlink["*"]
+			with lock:
+				for id in response["query"]["pages"]:
+					if "extlinks" in response["query"]["pages"][id]:
+						#print response["query"]["pages"][id]["title"]
+						for extlink in response["query"]["pages"][id]["extlinks"]:
+							links.append((response["query"]["pages"][id]["title"], extlink["*"]))
 	
-		if "continue" in response:
-			if response["continue"]["continue"] == "gapcontinue||":
-				gap_continue = response["continue"]["gapcontinue"]
-				le_offset = 0
+			if "continue" in response:
+				if response["continue"]["continue"] == "gapcontinue||":
+					gap_continue = response["continue"]["gapcontinue"]
+					le_offset = 0
+				else:
+					le_offset = response["continue"]["eloffset"]
 			else:
-				le_offset = response["continue"]["eloffset"]
+				gap_continue = None
 		else:
-			gap_continue = None
-
+			time.sleep(10)
 
 main()
