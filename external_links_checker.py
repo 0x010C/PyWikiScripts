7bb4979e8d6b0f0b4dc4b6d094aa2f706c70f354
Antoine Lamielle <gh@0x010c.fr>
2016-06-01 00:50:47 +0200

    manage a blacklist + display nb links found

--- external_links_checker.py
+++ external_links_checker.py
@@ -8,24 +8,34 @@ import pywiki
 import requests
 import threading
 import time
+import re
 from collections import deque
 
 
 lock = threading.Lock()
 links = deque()
 nb_article_crawled = 0
+nb_links_found = 0
+nb_links_found_lock = threading.Lock()
+
+blacklist = [
+	"wikimedia.org",
+	"wikipedia.org",
+	"www.minorplanetcenter.net",
+]
 
 class LinkChecker (threading.Thread):
 	def __init__(self, threadID):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
 	def run(self):
+		global nb_links_found
 		print "Starting " + str(self.threadID)
 		while True:
 			with lock:
 				try:
 					(title, link) = links.popleft()
-					print str(nb_article_crawled)+"#"+str(len(links))
+					print str(nb_links_found)+"#"+str(nb_article_crawled)+"#\t"+str(len(links))
 				except IndexError:
 					print "pause"+str(self.threadID)
 					link = None
@@ -33,14 +43,22 @@ class LinkChecker (threading.Thread):
 				time.sleep(2)
 				continue
 			
+			found = False
 			try:
 				r = requests.get(link)
 				if r.status_code != 200:
 					print title+" --> ("+str(r.status_code)+") "+link
+					found = True
 			except requests.exceptions.ConnectionError:
 				print title+" --> (DOM) "+link
+				found = True
 			except requests.exceptions.TooManyRedirects:
 				print title+" --> (RED) "+link
+				found = True
+			
+			if found:
+				with nb_links_found_lock:
+					nb_links_found += 1
 			
 		print "Exiting " + str(self.threadID)
 
@@ -52,8 +70,13 @@ def main():
 	pw.login()
 	pw.limit = 500
 	
+	blacklist_r = "("
+	for site in blacklist:
+		blacklist_r += site + "|"
+	blacklist_r = re.compile(blacklist_r[:-1]+")")
+
 	threads = []
-	for i in range(0,256):
+	for i in range(0,392):
 		threads.append(LinkChecker(i))
 		threads[i].start()
 	
@@ -83,9 +106,9 @@ def main():
 			with lock:
 				for id in response["query"]["pages"]:
 					if "extlinks" in response["query"]["pages"][id]:
-						#print response["query"]["pages"][id]["title"]
 						for extlink in response["query"]["pages"][id]["extlinks"]:
-							links.append((response["query"]["pages"][id]["title"], extlink["*"]))
+							if not blacklist_r.search(extlink["*"]):
+								links.append((response["query"]["pages"][id]["title"], extlink["*"]))
 	
 			if "continue" in response:
 				if response["continue"]["continue"] == "gapcontinue||":
